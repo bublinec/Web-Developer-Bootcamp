89a108c1756b615364cbe9fd51c118f31ccfeb8f
Juraj-Bublinec <jurajbublinec@gmail.com>
2020-02-26 14:48:55 +0000

    Add factorial

--- JavaScript-Basics/functions.js
+++ JavaScript-Basics/functions.js
@@ -8,12 +8,33 @@ function isEven(n)
 }
 
 
-function factIterative(n)
+function factorialIterative(n)
 {
-
+    var s = 1;
+    // corner case
+    if (n == 0)
+        return 1;
+    
+    while(n>1)
+    {
+        s *= n;
+        n -= 1;
+    }
+    return s;
 }
 
 
+function factorialRecursive(n)
+{
+    // BASE CASE
+    if (n == 0)
+        return 1;
+    // INDUCTIVE STEP
+    else
+        return n*factorialRecursive(n-1);
+}
 
 // Test
 console.log(isEven(4))
+console.log(factorialIterative(0))
+console.log(factorialRecursive(5))
