a0bef2c808b8b7516401c5acda6281f7804f392d
Juraj-Bublinec <jurajbublinec@gmail.com>
2020-02-27 16:10:08 +0000

    Add isUniform, remove htmls (node installed)

--- JavaScript-Basics/arrays.js
+++ JavaScript-Basics/arrays.js
@@ -1,11 +1,25 @@
 function printReverse(array)
 {
-    for(var i = array.length; i>=0; i--)
+    for(var i=array.length; i>=0; i--)
     {
         console.log(array[i]);
     }
 }
 
+function isUniform(array)
+{
+    for(var i=0; i<array.length-1; i++)
+    {
+        if(array[i]!=array[i+1])
+        {
+            return false
+        }
+    }
+    return true
+}
+
 
 // Tests
 printReverse([0,1,2,3,4,5]);
+console.log(isUniform([1,2,3,4,5]))
+console.log(isUniform([1,1,1,1,1]))
